a8db692f31ae943e26f94fea7c2cd2e222b5a6d4
Mateus <sirvergamer2@gmail.com>
2022-06-25 10:23:48 -0300

    cursojs

---
+++ aula11/ex008.js
@@ -0,0 +1,8 @@
+var vel = 60.5
+console.log ('A velocidade do carro é: ' + vel)
+
+if (vel > 60) { //condição simples 
+    console.log ('Você ultrapassou a velocidade permitida, MULTADO!')
+}
+
+console.log('Dirija sempre usando cinto de segurança')
---
+++ aula11/ex009.js
@@ -0,0 +1,10 @@
+var pais = 'EUA'
+
+console.log('Vivendo em ' + pais)
+
+if (pais == brasil) {
+    console.log ('Você é Brasileiro')
+}
+else {
+    console.log ('Você é Estrangeiro')
+}
